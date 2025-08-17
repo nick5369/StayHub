897de3b9e3414b6d4ef4e77dde722cd159c0a989
Nick Patel <nickpatel17042006@gmail.com>
2025-08-18 00:02:37 +0530

    final hero component

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,10 +1,66 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets, cities } from '../assets/assets'
 
 function Hero() {
   return (
-    <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen w-screen'>
+    <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("/src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen w-full relative'>
+      {/* Dark overlay for better text readability */}
+      <div className="absolute inset-0 bg-black/30"></div>
       
+      <div className="relative z-10">
+        <div className='bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20 text-sm backdrop-blur-sm text-left w-fit'>
+          The Ultimate Hotel Experience
+        </div>
+        <h1 className='font-playfair text-3xl md:text-5xl lg:text-6xl font-bold md:font-extrabold max-w-2xl mt-4 leading-tight text-left'>
+          Discover Your Perfect Gateway Destination
+        </h1>
+        <p className='max-w-xl mt-4 text-sm md:text-base opacity-90 leading-relaxed text-left'>
+          Unparalleled luxury and comfort await at the world's most exclusive hotels and resorts. Start your journey today.
+        </p>
+
+
+        <form className='bg-white text-gray-500 rounded-lg px-6 py-4  flex flex-col md:flex-row max-md:items-start gap-4 max-md:mx-auto mt-8'>
+
+            <div>
+                <div className='flex items-center gap-2'>
+                    <img src={assets.calenderIcon} className='h-4' />
+                    <label htmlFor="destinationInput">Destination</label>
+                </div>
+                <input list='destinations' id="destinationInput" type="text" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" placeholder="Type here" required />
+                <datalist id="destinations">
+                  {cities.map((city,index)=>(
+                    <option value={city} key={index}/>
+                  ))}
+                </datalist>
+            </div>
+
+            <div>
+                <div className='flex items-center gap-2'>
+                    <img src={assets.calenderIcon} className='h-4' />
+                    <label htmlFor="checkIn">Check in</label>
+                </div>
+                <input id="checkIn" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
+            </div>
+
+            <div>
+                <div className='flex items-center gap-2'>
+                    <img src={assets.calenderIcon} className='h-4' />
+                    <label htmlFor="checkOut">Check out</label>
+                </div>
+                <input id="checkOut" type="date" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none" />
+            </div>
+
+            <div className='flex md:flex-col max-md:gap-2 max-md:items-center'>
+                <label htmlFor="guests">Guests</label>
+                <input min={1} max={4} id="guests" type="number" className=" rounded border border-gray-200 px-3 py-1.5 mt-1.5 text-sm outline-none  max-w-16" placeholder="0" />
+            </div>
+
+            <button className='flex items-center justify-center gap-1 rounded-md bg-black py-3 px-4 text-white my-auto cursor-pointer max-md:w-full max-md:py-1' >
+                <img src={assets.searchIcon} className='h-7' />
+                <span>Search</span>
+            </button>
+        </form>
+      </div>
     </div>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -32,20 +32,20 @@ const Navbar = () => {
                 {/* Desktop Nav */}
                 <div className="hidden md:flex items-center gap-4 lg:gap-8">
                     {navLinks.map((link, i) => (
-                        <a key={i} href={link.path} className={`group flex flex-col gap-0.5 ${isScrolled ? "text-gray-700" : "text-white"}`}>
+                        <a key={i} href={link.path} className={`group flex flex-col gap-0.5 text-lg font-medium ${isScrolled ? "text-gray-700" : "text-white"}`}>
                             {link.name}
                             <div className={`${isScrolled ? "bg-gray-700" : "bg-white"} h-0.5 w-0 group-hover:w-full transition-all duration-300`} />
                         </a>
                     ))}
-                    <button className={`border px-4 py-1 text-sm font-light rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-white'} transition-all`}>
+                    <button className={`border px-4 py-1 text-lg font-medium rounded-full cursor-pointer ${isScrolled ? 'text-black' : 'text-white'} transition-all`}>
                         Dashboard
                     </button>
                 </div>
 
                 {/* Desktop Right */}
                 <div className="hidden md:flex items-center gap-4">
-                    <img src={assets.searchIcon} alt="search" className={`${isScrolled && 'invert'} h-7 transition-all duration-500`} ></img>
-                    <button className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${isScrolled ? "text-white bg-black" : "text-white bg-black"}`}>
+                    <img src={assets.searchIcon} alt="search" className={`${isScrolled && 'invert'} h-8 transition-all duration-500`} ></img>
+                    <button className={`px-8 py-2.5 rounded-full ml-4 text-lg font-medium transition-all duration-500 ${isScrolled ? "text-white bg-black" : "text-white bg-black"}`}>
                         Login
                     </button>
                 </div>
@@ -56,22 +56,22 @@ const Navbar = () => {
                 </div>
 
                 {/* Mobile Menu */}
-                <div className={`fixed top-0 left-0 w-full h-screen bg-white text-base flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
+                <div className={`fixed top-0 left-0 w-full h-screen bg-white text-xl flex flex-col md:hidden items-center justify-center gap-6 font-medium text-gray-800 transition-all duration-500 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"}`}>
                     <button className="absolute top-4 right-4" onClick={() => setIsMenuOpen(false)}>
                         <img src={assets.closeIcon} className="h-6.5" />
                     </button>
 
                     {navLinks.map((link, i) => (
-                        <a key={i} href={link.path} onClick={() => setIsMenuOpen(false)}>
+                        <a key={i} href={link.path} onClick={() => setIsMenuOpen(false)} className="text-xl font-medium">
                             {link.name}
                         </a>
                     ))}
 
-                    <button className="border px-4 py-1 text-sm font-light rounded-full cursor-pointer transition-all">
+                    <button className="border px-4 py-1 text-lg font-medium rounded-full cursor-pointer transition-all">
                         Dashboard
                     </button>
 
-                    <button className="bg-black text-white px-8 py-2.5 rounded-full transition-all duration-500">
+                    <button className="bg-black text-white px-8 py-2.5 rounded-full text-lg font-medium transition-all duration-500">
                         Login
                     </button>
                 </div>
