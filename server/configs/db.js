b39fa6f8dd64d6cf0ccae62d1d07124a1777a6d1
Nick Patel <nickpatel17042006@gmail.com>
2025-12-07 18:34:52 +0530

    mongouri error fixed

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,9 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/hotel-booking`)
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log('MongoDB connected successfully');
     } catch (error) {
-        console.log(error.message);
+        console.log('MongoDB connection error:', error.message);
     }
 }
 export default connectDB;
