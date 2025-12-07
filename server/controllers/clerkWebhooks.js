8a51fbeff9b894f90ab97a036179e6e75361134a
Nick Patel <nickpatel17042006@gmail.com>
2025-12-07 12:52:06 +0530

    project completed

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -15,20 +15,27 @@ const clerkWebhooks = async (req, res) => {
 
         const { type, data } = req.body;
 
-        const userData = {
-            _id: data.id,
-            email: data.email_addresses[0].email_address,
-            username: data.first_name + " " + data.last_name,
-            image: data.image_url,
-        }
-
         switch (type) {
-            case "user.created":
+            case "user.created": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                }
                 await User.create(userData);
                 break;
-            case "user.updated":
+            }
+            case "user.updated": {
+                const userData = {
+                    _id: data.id,
+                    email: data.email_addresses[0].email_address,
+                    username: data.first_name + " " + data.last_name,
+                    image: data.image_url,
+                }
                 await User.findByIdAndUpdate(data.id, userData);
                 break;
+            }
             case "user.deleted":
                 await User.findByIdAndDelete(data.id);
                 break;
